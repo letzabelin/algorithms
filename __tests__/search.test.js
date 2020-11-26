7107da9d8f69b7fcb45802ef97395966ec47f80c
Andrew Zabelin <letzabelin@gmail.com>
2020-11-26 20:38:59 +0300

    update tests

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -2,19 +2,15 @@ import { binarySearch } from '../src';
 
 const sortedArray = Array.from({ length: 20 }, (_, i) => i + 1);
 
-describe('array includes value', () => {
-  test('first test', () => {
-    expect(binarySearch(sortedArray, 5)).toBe(4);
-    expect(binarySearch(sortedArray, 1)).toBe(0);
-    expect(binarySearch(sortedArray, 20)).toBe(sortedArray.length - 1);
-  });
+test('array includes value', () => {
+  expect(binarySearch(sortedArray, 5)).toBe(4);
+  expect(binarySearch(sortedArray, 1)).toBe(0);
+  expect(binarySearch(sortedArray, 20)).toBe(sortedArray.length - 1);
 });
 
-describe('array does not include value', () => {
-  test('first test', () => {
-    expect(binarySearch(sortedArray, 30)).toBe(-1);
-    expect(binarySearch(sortedArray, -5)).toBe(-1);
-  });
+test('array does not include value', () => {
+  expect(binarySearch(sortedArray, 30)).toBe(-1);
+  expect(binarySearch(sortedArray, -5)).toBe(-1);
 });
 
 
